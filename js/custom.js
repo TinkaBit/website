34c504906bec9a7287860ef4a868fa373d89ca29
DimasDark <dimasdark@gmail.com>
2018-03-08 18:11:24 -0300

    Alterações visuais, efeitos, T, redesign das partes princiais

--- js/custom.js
+++ js/custom.js
@@ -1,97 +1,331 @@
-jQuery('#show1').bind('inview', function (event, visible) {
-    if (visible == true) {
-        jQuery(this).addClass("animated fadeInRightBig");
+/*--------------------------- MAGIC TEXT ----------------------------------------*/
+ var addEvent = function(object, type, callback) {
+    if (object == null || typeof(object) == 'undefined') return;
+    if (object.addEventListener) {
+        object.addEventListener(type, callback, false);
+    } else if (object.attachEvent) {
+        object.attachEvent("on" + type, callback);
     } else {
-        jQuery(this).removeClass("animated fadeInRightBig");
-			//jQuery('.effect-box').unbind('inview');
+        object["on"+type] = callback;
     }
+ };
+
+ var utils = {
+  norm: function(value, min, max) {
+    return (value - min) / (max - min);
+  },
+
+  lerp: function(norm, min, max) {
+    return (max - min) * norm + min;
+  },
+
+  map: function(value, sourceMin, sourceMax, destMin, destMax) {
+    return utils.lerp(utils.norm(value, sourceMin, sourceMax), destMin, destMax);
+  },
+
+  clamp: function(value, min, max) {
+    return Math.min(Math.max(value, Math.min(min, max)), Math.max(min, max));
+  },
+
+  distance: function(p0, p1) {
+    var dx = p1.x - p0.x,
+      dy = p1.y - p0.y;
+    return Math.sqrt(dx * dx + dy * dy);
+  },
+
+  distanceXY: function(x0, y0, x1, y1) {
+    var dx = x1 - x0,
+      dy = y1 - y0;
+    return Math.sqrt(dx * dx + dy * dy);
+  },
+
+  circleCollision: function(c0, c1) {
+    return utils.distance(c0, c1) <= c0.radius + c1.radius;
+  },
+
+  circlePointCollision: function(x, y, circle) {
+    return utils.distanceXY(x, y, circle.x, circle.y) < circle.radius;
+  },
+
+  pointInRect: function(x, y, rect) {
+    return utils.inRange(x, rect.x, rect.x + rect.radius) &&
+      utils.inRange(y, rect.y, rect.y + rect.radius);
+  },
+
+  inRange: function(value, min, max) {
+    return value >= Math.min(min, max) && value <= Math.max(min, max);
+  },
+
+  rangeIntersect: function(min0, max0, min1, max1) {
+    return Math.max(min0, max0) >= Math.min(min1, max1) &&
+      Math.min(min0, max0) <= Math.max(min1, max1);
+  },
+
+  rectIntersect: function(r0, r1) {
+    return utils.rangeIntersect(r0.x, r0.x + r0.width, r1.x, r1.x + r1.width) &&
+      utils.rangeIntersect(r0.y, r0.y + r0.height, r1.y, r1.y + r1.height);
+  },
+
+  degreesToRads: function(degrees) {
+    return degrees / 180 * Math.PI;
+  },
+
+  radsToDegrees: function(radians) {
+    return radians * 180 / Math.PI;
+  },
+
+  randomRange: function(min, max) {
+    return min + Math.random() * (max - min);
+  },
+
+  randomInt: function(min, max) {
+    return min + Math.random() * (max - min + 1);
+  },
+
+  getmiddle: function(p0, p1) {
+    var x = p0.x,
+      x2 = p1.x;
+    middlex = (x + x2) / 2;
+    var y = p0.y,
+      y2 = p1.y;
+    middley = (y + y2) / 2;
+    pos = [middlex, middley];
+
+    return pos;
+  },
+
+  getAngle: function(p0, p1) {
+    var deltaX = p1.x - p0.x;
+    var deltaY = p1.y - p0.y;
+    var rad = Math.atan2(deltaY, deltaX);
+    return rad;
+  },
+  inpercentW: function(size) {
+    return (size * W) / 100;
+  },
+
+  inpercentH: function(size) {
+    return (size * H) / 100;
+  },
+
+}
+
+canvas = document.getElementById("canvas");
+header = document.getElementById("masterheader");
+var ctx = canvas.getContext('2d');
+
+gridX = 5;
+gridY = 5;
+
+canvas.style.width ='100%';
+canvas.style.height='100%';
+
+W = canvas.width = canvas.offsetWidth;
+H = canvas.height = canvas.offsetHeight;
+
+//Function to redraw ~d+
+addEvent(window, "resize", function(event) {
+    message = new shape(W/2 , H/2 + 150, fieldvalue.value);
+    W = canvas.width = canvas.offsetWidth;
+    H = canvas.height = canvas.offsetHeight;
+    change();
+    console.log("Width: " + W + " " + "Height: " + H);
 });
 
-//  COMMENT 	
+function shape(x, y, texte) {
+  this.x = x;
+  this.y = y;
+  if (W < 768) {
+    this.size = 300;
+  } else {
+    this.size = 500;
+  }
+  this.text = texte;
+  this.placement = [];
+  this.vectors = [];
 
-//  COMMENT 
-jQuery('#show2').bind('inview', function (event, visible) {
-    if (visible == true) {
-        jQuery(this).addClass("animated fadeInLeftBig");
-    } else {
-        jQuery(this).removeClass("animated fadeInLeftBig");
-		//jQuery('.effect-box').unbind('inview');
+}
+
+shape.prototype.getValue = function() {
+  // Draw the shape
+  ctx.textAlign = "center";
+  ctx.font = "bold " + this.size + "px arial";
+  ctx.fillText(this.text, this.x, this.y);
+
+  var idata = ctx.getImageData(0, 0, W, H);
+
+  var buffer32 = new Uint32Array(idata.data.buffer);
+
+  for (var y = 0; y < H; y += gridY) {
+    for (var x = 0; x < W; x += gridX) {
+
+      if (buffer32[y * W + x]) {
+        this.placement.push(new particle(x, y));
+      }
     }
- });
+  }
+  ctx.clearRect(0, 0, W, H);
+}
 
-jQuery('#show3').bind('inview', function (event, visible) {
-        if (visible == true) {
-            jQuery('#show3 .feature-box').addClass("animated bounceIn");
+colors = [
+  '#30AC1F', '#6ab04c', '#009432', '#3ae374', '#28a745',
+  '#30AC1F'
+];
 
-            
-        }else{
-            jQuery('#show3 .feature-box').removeClass("animated bounceIn");
+function particle(x, y, type) {
+  this.radius = 1.1;
+  this.futurRadius = utils.randomInt(radius, radius+3);
+  
+  
+  this.rebond = utils.randomInt(1, 5);
+  this.x = x;
+  this.y = y;
+  
+  this.dying = false;
+  
+  this.base = [x, y]
 
-            //jQuery('.effect-box').unbind('inview');
-        }
-    });
+  this.vx = 0;
+  this.vy = 0;
+  this.type = type;
+  this.friction = .99;
+  this.gravity = gravity;
+  this.color = colors[Math.floor(Math.random() * colors.length)];
 
-/*--------------------------- VIDEO COVER ----------------------------------------*/
-//jQuery is required to run this code
-$( document ).ready(function() {
+  this.getSpeed = function() {
+    return Math.sqrt(this.vx * this.vx + this.vy * this.vy);
+  };
 
-    scaleVideoContainer();
+  this.setSpeed = function(speed) {
+    var heading = this.getHeading();
+    this.vx = Math.cos(heading) * speed;
+    this.vy = Math.sin(heading) * speed;
+  };
 
-    initBannerVideoSize('.video-container .poster img');
-    initBannerVideoSize('.video-container .filter');
-    initBannerVideoSize('.video-container video');
+  this.getHeading = function() {
+    return Math.atan2(this.vy, this.vx);
+  };
 
-    $(window).on('resize', function() {
-        scaleVideoContainer();
-        scaleBannerVideoSize('.video-container .poster img');
-        scaleBannerVideoSize('.video-container .filter');
-        scaleBannerVideoSize('.video-container video');
-    });
+  this.setHeading = function(heading) {
+    var speed = this.getSpeed();
+    this.vx = Math.cos(heading) * speed;
+    this.vy = Math.sin(heading) * speed;
+  };
 
-});
+  this.angleTo = function(p2) {
+    return Math.atan2(p2.y - this.y, p2.x - this.x);
 
-function scaleVideoContainer() {
+  };
 
-    var height = $(window).height() + 5;
-    var unitHeight = parseInt(height) + 'px';
-    $('.homepage-hero-module').css('height',unitHeight);
+  this.update = function(heading) {
+    this.x += this.vx;
+    this.y += this.vy;
+    this.vy += gravity;
+
+    this.vx *= this.friction;
+    this.vy *= this.friction;
+    
+    if(this.radius < this.futurRadius && this.dying === false){
+      this.radius += duration;
+    }else{
+      this.dying = true;
+    }
+      
+    if(this.dying === true){
+      this.radius -= duration; 
+    }
+
+    ctx.beginPath();
+
+    ctx.fillStyle = this.color;
+
+    ctx.arc(this.x, this.y, this.radius, Math.PI * 2, false);
+    ctx.fill();
+    ctx.closePath();
+
+    if (this.y < 0 || this.radius < 1) {
+      this.x = this.base[0];
+      this.dying = false;
+      this.y = this.base[1];
+      this.radius = 1.1;
+      this.setSpeed(.1);
+  this.futurRadius = utils.randomInt(radius, radius+3);
+      this.setHeading(utils.randomInt(utils.degreesToRads(0), utils.degreesToRads(360)));
+    }
+
+  };
+
+  this.setSpeed(utils.randomInt(.1, .5));
+  this.setHeading(utils.randomInt(utils.degreesToRads(0), utils.degreesToRads(360)));
 
 }
 
-function initBannerVideoSize(element){
+fieldvalue = document.getElementById("message");
+gravity = parseFloat(0);
+duration =  parseFloat(.4);
+resolution = parseFloat(5);
+speed = parseFloat(.2);
+radius = parseFloat(1);
 
-    $(element).each(function(){
-        $(this).data('height', $(this).height());
-        $(this).data('width', $(this).width());
-    });
+var message = new shape(W/2 , H/2 + 150, fieldvalue.value);
 
-    scaleBannerVideoSize(element);
+message.getValue();
 
+update();
+
+function change() {
+  ctx.clearRect(0, 0, W, H);
+  // gridX = parseFloat(element4.value);
+  // gridY = parseFloat(element4.value);
+  message.placement = [];
+  message.text = fieldvalue.value;
+  message.getValue();
 }
 
-function scaleBannerVideoSize(element){
+var fps = 100;
+function update() {
+  setTimeout(function() {
+    ctx.clearRect(0, 0, W, H);
+    for (var i = 0; i < message.placement.length; i++) {
+      message.placement[i].update();
+    }
 
-    var windowWidth = $(window).width(),
-    windowHeight = $(window).height() + 5,
-    videoWidth,
-    videoHeight;
-
-    // console.log(windowHeight);
-
-    $(element).each(function(){
-        var videoAspectRatio = $(this).data('height')/$(this).data('width');
-
-        $(this).width(windowWidth);
-
-        if(windowWidth < 1000){
-            videoHeight = windowHeight;
-            videoWidth = videoHeight / videoAspectRatio;
-            $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
-
-            $(this).width(videoWidth).height(videoHeight);
-        }
-
-        $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
-
-    });
+    requestAnimationFrame(update);
+  }, 1000 / fps);
 }
+
+/*--------------------------- TIMELINE ----------------------------------------*/
+
+(function() {
+
+  'use strict';
+
+  // define variables
+  var items = document.querySelectorAll(".timeline li");
+
+  // check if an element is in viewport
+  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+      if (isElementInViewport(items[i])) {
+        items[i].classList.add("in-view");
+      }
+    }
+  }
+
+  // listen for events
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
+
+})();
